c47285b83870af9b21f8365cdd466ab412aa7a95
zhanglibin.buaa <zhanglibin.buaa@bytedance.com>
2022-03-09 18:00:12 +0800

    add convnext

---
+++ configs/_base_/datasets/tianchi_Aug.py
@@ -0,0 +1,63 @@
+# dataset settings
+from mmsegmentation.mmseg.datasets.pipelines.transforms import RandomRotate
+
+
+dataset_type = 'TianchiDataset'  # 上一步中你定义的数据集的名字
+data_root = 'data/tianchi_aug'  # 数据集存储路径
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)  # 数据集的均值和标准差，空引用默认的，也可以网上搜代码计算
+crop_size = (768, 768)  # 数据增强时裁剪的大小
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(type='RandomMosaic', prob=0.8, img_scale=(1024, 1024)),
+    dict(type='Resize', img_scale=(1024, 1024),
+         ratio_range=(0.5, 2.0)),  # img_scale图像尺寸
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    # dict(type='RandomRotate', prob = 0.5, degree=90)
+    # dict(type='RandomCutOut', 
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1024, 1024),  # img_scale图像尺寸
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    type='MultiImageMixDataset',
+    samples_per_gpu=16,  # batch_size
+    workers_per_gpu=4,  # nums gpu
+    train=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='images/training',  # 训练图像路径
+        ann_dir='annotations/training',  # 训练mask路径
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='images/validation',  # 验证图像路径
+        ann_dir='annotations/validation',  # 验证mask路径
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='test/img',  # 测试图像路径
+        ann_dir=None,  # 测试mask路径
+        pipeline=test_pipeline)
+)
--- self_tools/train.sh
+++ self_tools/train.sh
@@ -2,9 +2,14 @@
 
 
 
-
+./tools/dist_train.sh ${CONFIG_FILE} ${GPU_NUM} [optional arguments]
 
 python tools/test.py ${CONFIG_FILE} ${CHECKPOINT_FILE} [--out ${RESULT_FILE}] [--eval ${EVAL_METRICS}] [--show]
 
 python3 tools/test.py configs/segformer/segformer_mit-b5_1024x1024_40k_tianchi_aug.py work_dirs/segformer_mit-b5_1024x1024_40k_tianchi_aug/iter_8000.pth --out data/test_res_dir/test_res/2507.pkl --aug-test --format-only
 ./tools/dist_test.sh configs/segformer/segformer_mit-b5_1024x1024_40k_tianchi_aug.py work_dirs/segformer_mit-b5_1024x1024_40k_tianchi_aug/iter_8000.pth  8 --aug-test --format-only --eval-options "imgfile_prefix=./segformer_test_results"
+
+
+./tools
+
+./tools/dist_train.sh  configs/segformer/segformer_mit-b5_1024x1024_40k_tianchi_aug_class_weight1_3_Aug.py 4 
